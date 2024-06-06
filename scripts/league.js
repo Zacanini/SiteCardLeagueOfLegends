180d4b3f1b23e5310e8276fd5788c533fefd3018
Matheus De O Zacanini <zacanini2@gmail.com>
2024-06-06 16:06:01 -0300

    APLICANDO JAVASCRIPT

--- scripts/league.js
+++ scripts/league.js
@@ -1,25 +1,72 @@
 async function buscarCampeoes() {
-    const response = await fetch('lol.json');
+  try {
+    const response = await fetch('./dragonTail/14.11.1/data/pt_BR/championFull.json');
     const data = await response.json();
     return data;
+  } catch (error) {
+    console.error('Erro ao buscar dados dos campeões:', error);
+    // Exibir mensagem de erro para o usuário (opcional)
   }
-  
-  function formatarDadosChampions(dados, nomeCampeao) {
-    const championData = dados.data[nomeCampeao];
-  
-    return {
-      name: championData.name,
-      title: championData.title,
-      stats: {
-        attack: championData.info.attack,
-        defense: championData.info.defense,
-        magic: championData.info.magic,
-        difficulty: championData.info.difficulty,
-        // ... adicione outras estatísticas conforme necessário
+}
+
+function obterDadosHabilidades(spells, tipoDado) {
+  const dadosHabilidades = [];
+
+  for (const habilidade of spells) {
+    dadosHabilidades.push(tipoDado === 'id' ? habilidade.id : habilidade.name);
+  }
+  return dadosHabilidades;
+}
+
+
+
+function formatarDadosChampionsAleatorio(dados) {
+  const championAleatorio = Object.keys(dados.data)[Math.floor(Math.random() * Object.keys(dados.data).length)];
+  const skinAleatoria = Object.keys(championAleatorio.skins)[Math.floor(Math.random() * Object.keys(championAleatorio.skins).length)];
+
+  const nomesHabilidades = obterDadosHabilidades(championAleatorio.spells, 'name');
+  const imagensHabilidades = obterDadosHabilidades(championAleatorio.spells, 'id');
+
+  return {
+    imagem: skinAleatoria.num,
+    nome: championAleatorio.id,
+    nameSkin: skinAleatoria.name,
+    habilidades: [
+      {
+        nome: nomesHabilidades[0],
+        imagem: imagensHabilidades[0],
       },
-      // ... adicione outras propriedades conforme necessário
-    };
-  }
-  
-  export { buscarCampeoes, formatarDadosChampions };
-  
+      {
+        nome: nomesHabilidades[1],
+        imagem: imagensHabilidades[1],
+      },
+      {
+        nome: nomesHabilidades[2],
+        imagem: imagensHabilidades[2],
+      },
+      {
+        nome: nomesHabilidades[3],
+        imagem: imagensHabilidades[3],
+      },
+      {
+        nome: nomesHabilidades[4],
+        imagem: imagensHabilidades[4],
+      },
+    ],
+    forca: {
+      ataque: championAleatorio.stats.attackdamage,
+      armadura: championAleatorio.stats.armor,
+      velocidadeMovimentacao: championAleatorio.stats.movespeed,
+      velocidadeAtaque: championAleatorio.stats.attackspeed,
+      regeneracaoVida: championAleatorio.stats.hpregen,
+      vida: championAleatorio.stats.hp,
+      resistenciaMagica: championAleatorio.stats.spellblock,
+    },
+    lore: {
+      historia: championAleatorio.lore.biography.content,
+      dicas: championAleatorio.allytips,
+    },
+  };
+}
+
+export { buscarCampeoes, formatarDadosChampionsAleatorio };
--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,9 @@
-import { buscarCampeoes, formatarDadosChampions } from './league.js';
+import { buscarCampeoes, formatarDadosChampionsAleatorio } from './league.js';
 
 async function getAatroxData() {
     try {
         const dados = await buscarCampeoes();
-        const Aatrox = formatarDadosChampions(dados, "Aatrox");
+        const Aatrox = formatarDadosChampionsAleatorio(dados);
         console.log(Aatrox);
     } catch (error) {
         console.error("Erro:", error); // Registre o erro no console
