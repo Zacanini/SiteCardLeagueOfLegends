1dc482962598ffe1e15f6de353b01336cb89a504
Matheus De O Zacanini <133885706+Zacanini@users.noreply.github.com>
2024-06-06 21:26:59 -0300

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,14 @@
-import { buscarCampeoes, formatarDadosChampionsAleatorio } from './league.js';
+import { getChampionData, formatarDadosChampions } from './league.js';
 
 async function getAatroxData() {
-    try {
-        const dados = await buscarCampeoes();
-        const Aatrox = formatarDadosChampionsAleatorio(dados);
-        console.log(Aatrox);
-    } catch (error) {
-        console.error("Erro:", error); // Registre o erro no console
-        // Lidar com o erro de forma adequada (por exemplo, exibir mensagem para o usuário)
-    }
+  try {
+    const dados = await getChampionData();
+    const Aatrox = formatarDadosChampions(dados);
+    console.log(Aatrox);
+} catch (error) {
+  console.error("Erro:", error); // Registre o erro no console
+  // Lidar com o erro de forma adequada (por exemplo, exibir mensagem para o usuário)
+}
 }
 
 getAatroxData();
